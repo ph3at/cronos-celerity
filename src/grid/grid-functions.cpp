5fecad0e8de45495e446ee7308f309c48b61abf4
Joshua Ocker <joshua.ocker@gmail.com>
2022-05-10 17:31:03 +0200

    Add grid functions for reading in grid from file, and comaring two grids.

--- src/grid/grid-functions.cpp
+++ src/grid/grid-functions.cpp
@@ -1,10 +1,11 @@
 #include <cmath>
+#include <fstream>
 
 #include "../misc/faces.h"
 #include "grid-functions.h"
 
 namespace GridFunctions {
-bool checkNaN(PaddedGrid<FieldStruct, GHOST_CELLS>& grid) {
+bool checkNaN(const PaddedGrid<FieldStruct, GHOST_CELLS>& grid) {
     for (unsigned x = 0; x < grid.xDim(); x++) {
         for (unsigned y = 0; y < grid.yDim(); y++) {
             for (unsigned z = 0; z < grid.zDim(); z++) {
@@ -17,5 +18,29 @@ bool checkNaN(PaddedGrid<FieldStruct, GHOST_CELLS>& grid) {
         }
     }
     return false;
+}
+
+SimpleGrid<FieldStruct> readFromFile(const std::string filename) {
+    std::ifstream inputFile(filename, std::ios::in);
+    if (!inputFile.is_open()) {
+        std::cerr << "File \"" << filename << "\" could not be opened." << std::endl;
+        return SimpleGrid<FieldStruct>({}, 0, 0, 0);
+    } else {
+        unsigned xDim, yDim, zDim;
+        inputFile >> xDim >> yDim >> zDim;
+        SimpleGrid<FieldStruct> result = SimpleGrid<FieldStruct>({}, xDim, yDim, zDim);
+        double value;
+        for (unsigned x = 0; x < xDim; x++) {
+            for (unsigned y = 0; y < yDim; y++) {
+                for (unsigned z = 0; z < zDim; z++) {
+                    for (unsigned field = 0; field < NUM_PHYSICAL_FIELDS; field++) {
+                        inputFile >> value;
+                        result(x, y, z)[field] = value;
+                    }
+                }
+            }
+        }
+        return result;
+    }
 }
 }; // namespace GridFunctions
--- src/grid/grid-functions.h
+++ src/grid/grid-functions.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cassert>
+#include <cmath>
+
 #include "padded-grid.h"
 #include "simple-grid.h"
 
@@ -7,11 +10,16 @@
 #include "../field-wrapper/phys-fields.h"
 
 namespace GridFunctions {
-bool checkNaN(PaddedGrid<FieldStruct, GHOST_CELLS>& grid);
-template <unsigned padding> void printGrid(PaddedGrid<FieldStruct, padding>& grid);
+bool checkNaN(const PaddedGrid<FieldStruct, GHOST_CELLS>& grid);
+template <unsigned padding> void printGrid(const PaddedGrid<FieldStruct, padding>& grid);
+template <unsigned padding1, unsigned padding2>
+void compare(const PaddedGrid<FieldStruct, padding1>& baseline,
+             const PaddedGrid<FieldStruct, padding2>& other);
+SimpleGrid<FieldStruct> readFromFile(const std::string filename);
 }; // namespace GridFunctions
 
-template <unsigned padding> void GridFunctions::printGrid(PaddedGrid<FieldStruct, padding>& grid) {
+template <unsigned padding>
+void GridFunctions::printGrid(const PaddedGrid<FieldStruct, padding>& grid) {
     std::cout.width(5);
     for (size_t x = 0; x < grid.xDim(); x++) {
         for (size_t y = 0; y < grid.yDim(); y++) {
@@ -30,3 +38,36 @@ template <unsigned padding> void GridFunctions::printGrid(PaddedGrid<FieldStruct
         std::cout << std::endl << std::endl;
     }
 }
+
+template <unsigned padding1, unsigned padding2>
+void GridFunctions::compare(const PaddedGrid<FieldStruct, padding1>& baseline,
+                            const PaddedGrid<FieldStruct, padding2>& other) {
+    assert(baseline.xEnd() - baseline.xStart() == other.xEnd() - other.xStart());
+    assert(baseline.yEnd() - baseline.yStart() == other.yEnd() - other.yStart());
+    assert(baseline.zEnd() - baseline.zStart() == other.zEnd() - other.zStart());
+    const int offset = padding2 - padding1;
+
+    double maxDiff = 0;
+    double sumSquaredError = 0;
+    for (int x = baseline.xStart(); x < static_cast<int>(baseline.xEnd()); x++) {
+        for (int y = baseline.yStart(); y < static_cast<int>(baseline.yEnd()); y++) {
+            for (int z = baseline.zStart(); z < static_cast<int>(baseline.zEnd()); z++) {
+                for (unsigned field = 0; field < NUM_PHYSICAL_FIELDS; field++) {
+                    double diff = std::abs(baseline(x, y, z)[field] -
+                                           other(offset + x, offset + y, offset + z)[field]);
+                    if (diff > maxDiff) {
+                        maxDiff = diff;
+                    }
+                    sumSquaredError += diff * diff;
+                }
+            }
+        }
+    }
+    double numValues = static_cast<double>(
+        (baseline.xEnd() - baseline.xStart()) * (baseline.yEnd() - baseline.yStart()) *
+        (baseline.zEnd() - baseline.zStart()) * NUM_PHYSICAL_FIELDS);
+    double rms = std::sqrt(sumSquaredError / numValues);
+    std::cout << "Root of mean squared error: " << rms << ", with a maximum deviation of "
+              << maxDiff << std::endl;
+}
+
