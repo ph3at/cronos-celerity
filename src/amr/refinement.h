19484edf6bf16b84ce93403b35b62d5050266db3
Joshua Ocker <joshua.ocker@gmail.com>
2022-05-24 22:44:08 +0200

    Implement function for calculating grid efficiency.

--- src/amr/refinement.h
+++ src/amr/refinement.h
@@ -5,6 +5,8 @@
 #include "amr-node.h"
 #include "flags.h"
 
+typedef std::array<std::pair<unsigned, unsigned>, 3> GridBoundary;
+
 template <class SolverType, class ProblemType, class Fields, unsigned padding> class Refinery {
   public:
     Refinery(const AMRParameters& configuration);
@@ -18,7 +20,8 @@ template <class SolverType, class ProblemType, class Fields, unsigned padding> c
 
     std::vector<std::vector<AMRNode<SolverType, ProblemType, Fields, padding>>> amrNodes;
 
-    std::vector<CellFlags::Flags> flagCells();
+    std::vector<CellFlags::Flags> flagCells() const;
+    double efficiency(const GridBoundary& grid, const CellFlags::Flags& flags) const;
 };
 
 template <class SolverType, class ProblemType, class Fields, unsigned padding>
@@ -31,7 +34,8 @@ void Refinery<SolverType, ProblemType, Fields, padding>::refine() {
 }
 
 template <class SolverType, class ProblemType, class Fields, unsigned padding>
-std::vector<CellFlags::Flags> Refinery<SolverType, ProblemType, Fields, padding>::flagCells() {
+std::vector<CellFlags::Flags>
+Refinery<SolverType, ProblemType, Fields, padding>::flagCells() const {
     const int maxLevel = static_cast<int>(this->amrNodes.size());
     std::vector<CellFlags::Flags> levelFlags;
     levelFlags.reserve(maxLevel);
@@ -48,6 +52,33 @@ std::vector<CellFlags::Flags> Refinery<SolverType, ProblemType, Fields, padding>
         CellFlags::addBuffer(levelFlags[level], this->configuration.bufferSize);
     }
     return levelFlags;
+}
+
+template <class SolverType, class ProblemType, class Fields, unsigned padding>
+double Refinery<SolverType, ProblemType, Fields, padding>::efficiency(
+    const GridBoundary& grid, const CellFlags::Flags& flags) const {
+    unsigned flaggedCells = 0;
+    for (unsigned x = std::max(grid[0].first, flags.first);
+         x < std::min(grid[0].second, flags.first + static_cast<unsigned>(flags.second.size()));
+         x++) {
+        const unsigned xx = x - flags.first;
+        const std::pair<unsigned, CellFlags::FlagsY>& flagsY = flags.second[xx];
+        for (unsigned y = std::max(grid[1].first, flagsY.first);
+             y <
+             std::min(grid[1].second, flagsY.first + static_cast<unsigned>(flagsY.second.size()));
+             y++) {
+            const unsigned yy = y - flagsY.first;
+            for (std::pair<unsigned, unsigned> pair : flagsY.second[yy]) {
+                const int left = std::max(pair.first, grid[3].first);
+                const int right = std::min(pair.second + 1, grid[2].second);
+                flaggedCells += std::max(0, right - left);
+            }
+        }
+    }
+    const double gridSize =
+        static_cast<double>((grid[0].second - grid[0].first) * (grid[1].second - grid[1].first) *
+                            (grid[2].second - grid[2].first));
+    return static_cast<double>(flaggedCells) / gridSize;
 }
 
 template <class SolverType, class ProblemType, class Fields, unsigned padding>
