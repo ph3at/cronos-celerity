1d5910566866981233a82850a91a9126490a1bde
Markus Wippler <markus.wippler@uibk.ac.at>
2023-07-07 12:32:28 +0200

    Change benchmark to do only a single run

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -18,9 +18,7 @@
 #include "../src/solver/runge-kutta-sycl-solver.h"
 
 template <typename Solver, typename... Args>
-auto benchmarkSolver(const int numSizes, const int numRuns, const std::string_view configPath, Args&... args) {
-    auto results = std::vector(numSizes, std::vector<std::chrono::milliseconds>(numRuns));
-
+auto benchmarkSolver(const int size, const std::string_view configPath, Args&... args) {
     toml::table config = toml::parse_file(configPath);
 
     const auto baseNumCellsX = config["grid"]["number_cells_x"].value_or(std::size_t{ 4 });
@@ -29,106 +27,97 @@ auto benchmarkSolver(const int numSizes, const int numRuns, const std::string_vi
 
     const auto scalingFactor = std::cbrt(2);
 
-    for (auto r = 0; r < numRuns; ++r) {
-        for (auto s = 0; s < numSizes; ++s) {
-            const auto numCellsX = static_cast<std::size_t>(std::round(baseNumCellsX * std::pow(scalingFactor, s)));
-            const auto numCellsY = static_cast<std::size_t>(std::round(baseNumCellsY * std::pow(scalingFactor, s)));
-            const auto numCellsZ = static_cast<std::size_t>(std::round(baseNumCellsZ * std::pow(scalingFactor, s)));
+    const auto numCellsX = static_cast<std::size_t>(std::round(baseNumCellsX * std::pow(scalingFactor, size)));
+    const auto numCellsY = static_cast<std::size_t>(std::round(baseNumCellsY * std::pow(scalingFactor, size)));
+    const auto numCellsZ = static_cast<std::size_t>(std::round(baseNumCellsZ * std::pow(scalingFactor, size)));
 
-            *(config["grid"]["number_cells_x"].as_integer()) = numCellsX;
-            *(config["grid"]["number_cells_y"].as_integer()) = numCellsY;
-            *(config["grid"]["number_cells_z"].as_integer()) = numCellsZ;
+    *(config["grid"]["number_cells_x"].as_integer()) = numCellsX;
+    *(config["grid"]["number_cells_y"].as_integer()) = numCellsY;
+    *(config["grid"]["number_cells_z"].as_integer()) = numCellsZ;
 
-            ShockTube shockTube(config);
+    ShockTube shockTube(config);
 
-            const auto timeStart = std::chrono::high_resolution_clock::now();
+    const auto timeStart = std::chrono::high_resolution_clock::now();
 
-            Solver solver(shockTube, args...);
-            solver.initialise();
+    Solver solver(shockTube, args...);
+    solver.initialise();
 
-            int numTimesteps = 0;
-            while (!solver.isFinished()) {
-                solver.step();
-                solver.adjust();
-                ++numTimesteps;
-            }
-            solver.finalise();
-
-            std::cout << "Num Timesteps: " << numTimesteps << std::endl;
-
-            const auto timeEnd = std::chrono::high_resolution_clock::now();
-            const auto elapsedMs = std::chrono::duration_cast<std::chrono::milliseconds>(timeEnd - timeStart);
-            results[s][r] = elapsedMs;
-        }
+    int numTimesteps = 0;
+    while (!solver.isFinished()) {
+        solver.step();
+        solver.adjust();
+        ++numTimesteps;
     }
+    solver.finalise();
 
-    return results;
+    const auto timeEnd = std::chrono::high_resolution_clock::now();
+    const auto elapsedMs = std::chrono::duration_cast<std::chrono::milliseconds>(timeEnd - timeStart);
+
+    std::cout << "Num Timesteps: " << numTimesteps << std::endl;
+
+    return elapsedMs;
 }
 
-void cpuBenchmark() {
-    const auto benchmarkResults = benchmarkSolver<RungeKuttaSolver<ShockTube, FieldStruct, GHOST_CELLS>>(
-        20, 10, "configuration/shock-tube-benchmark.toml");
+void cpuBenchmark(const int size) {
+    const auto benchmarkTimeMs = benchmarkSolver<RungeKuttaSolver<ShockTube, FieldStruct, GHOST_CELLS>>(
+        size, "configuration/shock-tube-benchmark.toml");
 
-    for (std::size_t s = 0; s < benchmarkResults.size(); ++s) {
-        for (std::size_t r = 0; r < benchmarkResults[s].size(); ++r) {
-            std::cout << "CPU: Size " << s << " run #" << r << " took " << benchmarkResults[s][r].count() << "ms"
-                      << std::endl;
-        }
-    }
+    std::cerr << "arch,size,time" << std::endl;
+    std::cerr << "cpu," << size << "," << benchmarkTimeMs.count() << std::endl;
 }
 
-void syclBenchmark() {
-    const auto benchmarkResults = benchmarkSolver<RungeKuttaSyclSolver<ShockTube, FieldStruct, GHOST_CELLS>>(
-        24, 10, "configuration/shock-tube-benchmark.toml");
+void syclBenchmark(const int size) {
+    const auto benchmarkTimeMs = benchmarkSolver<RungeKuttaSyclSolver<ShockTube, FieldStruct, GHOST_CELLS>>(
+        size, "configuration/shock-tube-benchmark.toml");
 
-    for (std::size_t s = 0; s < benchmarkResults.size(); ++s) {
-        for (std::size_t r = 0; r < benchmarkResults[s].size(); ++r) {
-            std::cout << "Sycl GPU: Size " << s << " run #" << r << " took " << benchmarkResults[s][r].count() << "ms"
-                      << std::endl;
-        }
-    }
+    std::cerr << "arch,size,time" << std::endl;
+    std::cerr << "sycl," << size << "," << benchmarkTimeMs.count() << std::endl;
 }
 
-void celerityBenchmark() {
+void celerityBenchmark(const int size) {
     auto queue = celerity::distr_queue();
-    const auto benchmarkResults = benchmarkSolver<RungeKuttaCeleritySolver<ShockTube, FieldStruct, GHOST_CELLS>>(
-        24, 10, "configuration/shock-tube-benchmark.toml", queue);
+    const auto benchmarkTimeMs = benchmarkSolver<RungeKuttaCeleritySolver<ShockTube, FieldStruct, GHOST_CELLS>>(
+        size, "configuration/shock-tube-benchmark.toml", queue);
 
-    queue.submit(celerity::allow_by_ref, [=, &benchmarkResults](celerity::handler& cgh) {
-        cgh.host_task(celerity::on_master_node, [=, &benchmarkResults] {
-            for (std::size_t s = 0; s < benchmarkResults.size(); ++s) {
-                for (std::size_t r = 0; r < benchmarkResults[s].size(); ++r) {
-                    CELERITY_INFO("Celerity GPU: Size {} run #{} took {}ms", s, r, benchmarkResults[s][r].count());
-                }
-            }
+    queue.submit(celerity::allow_by_ref, [=, &benchmarkTimeMs](celerity::handler& cgh) {
+        cgh.host_task(celerity::on_master_node, [=, &benchmarkTimeMs] {
+            std::cerr << "arch,size,time" << std::endl;
+            std::cerr << "celerity," << size << "," << benchmarkTimeMs.count() << std::endl;
         });
     });
     queue.slow_full_sync();
 }
 
 void printUsage(const std::string_view name) {
-    std::cout << "Usage: " << name << " cpu|sycl|celerity" << std::endl;
+    std::cout << "Usage: " << name << " cpu|sycl|celerity size" << std::endl;
 }
 
 int main(int argc, char* argv[]) {
     const auto arguments = std::vector<std::string>{ argv, argv + argc };
 
-    if (arguments.size() != 2) {
+    if (arguments.size() != 3) {
         printUsage(arguments[0]);
         return EXIT_FAILURE;
     }
 
     const auto benchmark = arguments[1];
+    const auto size = std::stoi(arguments[2]);
+
+    if (size < 0 || size > 100) {
+        std::cout << "ERROR - Invalid size value '" << size << "' should be between [0-100]" << std::endl;
+        printUsage(arguments[0]);
+        return EXIT_FAILURE;
+    }
 
     if (benchmark == "cpu") {
-        cpuBenchmark();
+        cpuBenchmark(size);
     } else if (benchmark == "sycl") {
-        syclBenchmark();
+        syclBenchmark(size);
     } else if (benchmark == "celerity") {
-        celerityBenchmark();
+        celerityBenchmark(size);
     } else {
         std::cout << "ERROR - Unknown benchmark type '" << benchmark << "'" << std::endl;
-        printUsage(arguments[1]);
+        printUsage(arguments[0]);
         return EXIT_FAILURE;
     }
 
